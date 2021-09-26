a6b9d13491d44901404e2b3ebff0bb9d583cf3e4
shu-kob <shukobuchi@gmail.com>
2021-09-26 21:50:31 +0900

    refactor getKeys.js and support ltc, doge

--- getKeys.js
+++ getKeys.js
@@ -5,21 +5,49 @@ const fs = require("fs");
 const { mnemonic } = require('./mnemonic.json');
 const MAINNET = bitcoin.networks.bitcoin;
 const TESTNET = bitcoin.networks.testnet;
-// let bitcoinNetwork = MAINNET;
-let bitcoinNetwork = TESTNET;
+
+const LITECOIN = {
+    messagePrefix: '\x19Litecoin Signed Message:\n',
+    bech32: 'ltc',
+    bip32: {
+      public: 0x019da462,
+      private: 0x019d9cfe,
+    },
+    pubKeyHash: 0x30,
+    scriptHash: 0x32,
+    wif: 0xb0,
+};
+
+const DOGECOIN = {
+    messagePrefix: '\x19Dogecoin Signed Message:\n',
+    bip32: {
+      public: 0x02facafd,
+      private: 0x02fac398
+    },
+    pubKeyHash: 0x1e,
+    scriptHash: 0x16,
+    wif: 0x9e
+};
+
+let bitcoinNetwork = TESTNET; // MAINNET, TESTNET, LITECOIN or DOGECOIN
+
+let purpose = "44"
 
 let coinType = null;
 
 if (bitcoinNetwork == MAINNET) {
     coinType = "0";
 }
-else if (bitcoinNetwork == TESTNET) {
-    coinType = "1";
+else if (bitcoinNetwork == LITECOIN) {
+    coinType = "2";
+}
+else if (bitcoinNetwork == DOGECOIN) {
+    coinType = "3";
 }
 
 let account = "0"
 
-const path = `m/44'/${coinType}'/${account}'`
+const path = `m/${purpose}'/${coinType}'/${account}'`
 
 console.log("path:\n" + path);
 
